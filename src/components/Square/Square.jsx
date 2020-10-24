29170f2f71894bf0bee29ea2b002582c62db6608
Thao Phung <ttp7428@truman.edu>
2020-10-24 14:44:26 -0500

    refactor check winning/losing status

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,42 +1,42 @@
 import React, { Component } from 'react';
 // import '../color_game.css';
 
-// const Square = ({color, children, setCorrectView, setWrongView }) => {
-//     return (
-//         <div
-//             onClick={ children === color ? setCorrectView(color) : setWrongView(color)}
-//             className="square"
-//             style={{ 
-//                 backgroundColor: color 
-//             }}
-//         >
-//         </div>
-//     )   
-// }
-
-class Square extends Component {
-    //render a square with different properties
-    render() {
-        return (
-            <div
-                onClick={this.changeColor}
-                className="square"
-                style={{ backgroundColor: this.props.backgroundColor }}
-                ref="square">
-            </div>
-        );
-    }
-
-    //sends parameters to formatSquareColor on ColorGame.jsx
-    changeColor = () => {
-        if (this.props.children === this.props.backgroundColor) {
-            this.props.squareColor(this.props.backgroundColor, true);
-        }
-        else {
-            this.props.squareColor(this.props.backgroundColor, false);
-            this.refs.square.style.visibility = "hidden";  //hides incorrect square onClick
-        }
-    }
+const Square = ({color, winningColor, onWinningViewChange, onLosingViewChange }) => {
+    return (
+        <div
+            onClick={ winningColor === color ? onWinningViewChange() : onLosingViewChange() }
+            className="square"
+            style={{ 
+                backgroundColor: color 
+            }}
+        >
+        </div>
+    )   
 }
+
+// class Square extends Component {
+//     //render a square with different properties
+//     render() {
+//         return (
+//             <div
+//                 onClick={this.changeColor}
+//                 className="square"
+//                 style={{ backgroundColor: this.props.backgroundColor }}
+//                 ref="square">
+//             </div>
+//         );
+//     }
+
+//     //sends parameters to formatSquareColor on ColorGame.jsx
+//     changeColor = () => {
+//         if (this.props.children === this.props.backgroundColor) {
+//             this.props.squareColor(this.props.backgroundColor, true);
+//         }
+//         else {
+//             this.props.squareColor(this.props.backgroundColor, false);
+//             this.refs.square.style.visibility = "hidden";  //hides incorrect square onClick
+//         }
+//     }
+// }
 
 export default Square;
--- src/container/App.jsx
+++ src/container/App.jsx
@@ -25,7 +25,21 @@ class App extends Component {
           onEasyModeChange={() => this.randomColorGen(3)}
           onHardModeChange={() => this.randomColorGen(6)}
         />
-        <div>{this.renderSquares()}</div>
+        <div>
+          <div className="container">
+            {this.state.colors.map((color) => (
+              <Square
+                key={color}
+                color={color}
+                winningColor = {this.state.winningColor}
+                onLosingViewChange = { () => this.onLosingViewChange}
+                onWinningViewChange = { () => this.onWinningViewChange}
+                squareColor={this.formatSquareColor}
+              >
+              </Square>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
@@ -50,37 +64,14 @@ class App extends Component {
     this.setState({ headingColor: "steelblue" });
   };
 
-  //change square color & heading
-  formatSquareColor = (clickedColor, sameColor) => {
-    if (sameColor === true) {
-      this.setState({ message: "Correct!" });
-      this.setState({ headingColor: this.state.winningColor }); //change heading color to winning color if correct
-    } else {
-      const colors = [...this.state.colors];
-      for (let index in colors) {
-        if (clickedColor === colors[index]) {
-          this.setState({ message: "Try Again" });
-        }
-      }
-    }
-  };
+  onWinningViewChange = (clickedColor) => {
+    this.setState({ message: "Correct!" });
+    this.setState({ headingColor: this.state.winningColor }); 
+  }
 
-  //render squares by mapping colors state into individual <Square/> elements
-  renderSquares = () => {
-    return (
-      <div className="container">
-        {this.state.colors.map((color) => (
-          <Square
-            key={color}
-            backgroundColor={color}
-            squareColor={this.formatSquareColor}
-          >
-            {this.state.winningColor}
-          </Square>
-        ))}
-      </div>
-    );
-  };
+  onLosingViewChange = (clickedColor) =>  {
+    this.setState({ message: "Try Again" });
+  }
 }
     
 export default App;
